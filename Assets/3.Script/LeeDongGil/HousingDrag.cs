d25a7ef78cc95bd0cb2e516a1596dfb0b22b67b4
hdy <lldgll@naver.com>
2024-02-08 13:15:15 +0900

    [Feat] Housing Grid System
    하우징 시스템 마우스 누른상태로 하우징 위로 마우스 이동 시 같이 움직이는 버그 수정
    하우징 시스템 그리드 움직임 구현

--- Assets/3.Script/LeeDongGil/HousingDrag.cs
+++ Assets/3.Script/LeeDongGil/HousingDrag.cs
@@ -9,24 +9,42 @@ public class HousingDrag : MonoBehaviour
     public Vector3 offset;
     public Transform previousParent;
 
+
+    [Header("Build Setting")]
     public GameObject space;
     public float spaceX;
     public float spaceY;
     private int checkMinusY = 1;
     private int checkMinusX = 1;
-
-    private SpriteRenderer check;
     public LayerMask canBuild;
     public LayerMask cannotBuild;
     public LayerMask buildingLayer;
+
+    private SpriteRenderer check;
+    private BoxCollider boxCollider;
+    [SerializeField] private BoxCollider subCollider;
+
+    #region Gizmos parameter
+/*
+    private Vector3 gizmosPosition;
+    private Vector3 gizmosDirection;
+    private float gizmosDistance;
+    private Ray gizmosRay;
+    private RaycastHit gizmosHit;
+*/
+    #endregion
 
     private void Start()
     {
         check = transform.GetChild(0).GetComponent<SpriteRenderer>();
         transform.position = new Vector3(transform.position.x, transform.position.y, -1);
         previousParent = transform.parent;
-
         space.transform.localScale = new Vector3(spaceX, spaceY, 1);
+        boxCollider = GetComponent<BoxCollider>();
+        subCollider = transform.GetChild(1).GetComponent<BoxCollider>();
+
+        boxCollider.size = new Vector3(spaceX, spaceY, 0.2f);
+        subCollider.enabled = false;
     }
 
     private void Update()
@@ -54,10 +72,26 @@ public class HousingDrag : MonoBehaviour
             {
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 RaycastHit hit;
-                if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
+                //gizmosRay = ray;
+
+
+                if (Physics.Raycast(ray, out hit))
                 {
-                    isDragging = true;
-                    offset = transform.position - ray.GetPoint(hit.distance);
+                    Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.green);
+                    if (hit.collider.gameObject == gameObject)
+                    {
+                        isDragging = true;
+                        offset = transform.position - ray.GetPoint(hit.distance);
+                    }
+
+                    #region Gizmos
+/*
+                    gizmosHit = hit;
+                    gizmosPosition = ray.origin;
+                    gizmosDirection = ray.direction;
+                    gizmosDistance = hit.distance;
+*/
+                    #endregion
                 }
             }
             else if (touch.phase == TouchPhase.Ended)
@@ -69,14 +103,13 @@ public class HousingDrag : MonoBehaviour
             if (isDragging)
             {
                 check.gameObject.SetActive(true);
+                subCollider.enabled = true;
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 Vector3 newPosition = ray.GetPoint(offset.z);
-
                 checkMinusX = newPosition.x >= 0 ? 1 : -1;
                 checkMinusY = newPosition.y >= 0 ? 1 : -1;
                 float moveX = spaceX % 2 == 0 ? Mathf.RoundToInt(Mathf.Abs(newPosition.x)) * checkMinusX : Mathf.FloorToInt(newPosition.x) + 0.5f;
                 float moveY = spaceY % 2 == 0 ? Mathf.RoundToInt(Mathf.Abs(newPosition.y)) * checkMinusY : Mathf.FloorToInt(newPosition.y) + 0.5f;
-                
                 transform.position = new Vector3(moveX, moveY, -1);
             }
         }
@@ -107,15 +140,16 @@ public class HousingDrag : MonoBehaviour
         float distance_Box = 20f;
 
         RaycastHit hit_RT, hit_LT, hit_LB, hit_RB, hit_Center, hit_Box;
+        int defaultLayer = ~(1 << LayerMask.NameToLayer("Default"));
 
-        bool isHitRT = Physics.Raycast(ray_RT, out hit_RT, distance_RT, ~(1 << LayerMask.NameToLayer("Default")));
-        bool isHitLT = Physics.Raycast(ray_LT, out hit_LT, distance_LT, ~(1 << LayerMask.NameToLayer("Default")));
-        bool isHitLB = Physics.Raycast(ray_LB, out hit_LB, distance_LB, ~(1 << LayerMask.NameToLayer("Default")));
-        bool isHitRB = Physics.Raycast(ray_RB, out hit_RB, distance_RB, ~(1 << LayerMask.NameToLayer("Default")));
-        bool isHitCenter = Physics.Raycast(ray_Center, out hit_Center, distance_Center, ~(1 << LayerMask.NameToLayer("Default")));
-        bool isHitBox = Physics.BoxCast(transform.position, new Vector3(spaceX * 0.495f, spaceY * 0.495f, 0.05f), Vector3.forward, out hit_Box, Quaternion.identity, distance_Box, ~(1 << LayerMask.NameToLayer("Default")));
+        bool isHitRT = Physics.Raycast(ray_RT, out hit_RT, distance_RT, defaultLayer);
+        bool isHitLT = Physics.Raycast(ray_LT, out hit_LT, distance_LT, defaultLayer);
+        bool isHitLB = Physics.Raycast(ray_LB, out hit_LB, distance_LB, defaultLayer);
+        bool isHitRB = Physics.Raycast(ray_RB, out hit_RB, distance_RB, defaultLayer);
+        bool isHitCenter = Physics.Raycast(ray_Center, out hit_Center, distance_Center, defaultLayer);
+        bool isHitBox = Physics.BoxCast(transform.position, new Vector3(spaceX * 0.495f, spaceY * 0.495f, 0.05f), Vector3.forward, out hit_Box, Quaternion.identity, distance_Box, defaultLayer);
 
-        
+
         distance_RT = hit_RT.distance;
         distance_LT = hit_LT.distance;
         distance_LB = hit_LB.distance;
@@ -137,6 +171,7 @@ public class HousingDrag : MonoBehaviour
                 transform.SetParent(hit_Center.collider.transform.parent);
                 transform.position = new Vector3(transform.position.x, transform.position.y, 0);
                 check.gameObject.SetActive(false);
+                subCollider.enabled = false;
             }
         }
         else
@@ -144,6 +179,11 @@ public class HousingDrag : MonoBehaviour
             isCanBuild = false;
             check.color = new Color32(255, 0, 0, 100);
             transform.SetParent(previousParent);
+            if(!isDragging)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, -0.5f);
+                subCollider.enabled = false;
+            }
         }
 
         #region Draw Ray
@@ -160,5 +200,26 @@ public class HousingDrag : MonoBehaviour
         #endregion
     }
 
+    //OnDrawGizmos
+/*
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
 
+        // 함수 파라미터 : 현재 위치, Box의 절반 사이즈, Ray의 방향, RaycastHit 결과, Box의 회전값, BoxCast를 진행할 거리
+        if (true == Physics.BoxCast(gizmosPosition, new Vector3(0.495f, 0.495f, 0.2f), gizmosDirection, out gizmosHit, Quaternion.identity))
+        {
+            // Hit된 지점까지 ray를 그려준다.
+            Gizmos.DrawRay(gizmosPosition, gizmosDirection * gizmosHit.distance);
+
+            // Hit된 지점에 박스를 그려준다.
+            Gizmos.DrawWireCube(gizmosPosition + gizmosDirection * gizmosHit.distance, new Vector3(0.495f, 0.495f, 0.2f) * 2);
+        }
+        else
+        {
+            // Hit가 되지 않았으면 최대 검출 거리로 ray를 그려준다.
+            Gizmos.DrawRay(gizmosPosition, gizmosDirection * 20.0f);
+        }
+    }
+*/
 }
