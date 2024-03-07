924144a641b06b34ee43c70293fe0d23bbdced3f
OhJungGeun <poolutoocaa123@gmail.com>
2024-03-07 18:00:07 +0900

    [Feat] DrawUI

--- Assets/3.Script/OhJungGeun/UISystem/DrawUISystem.cs
+++ Assets/3.Script/OhJungGeun/UISystem/DrawUISystem.cs
@@ -1,30 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class DrawUISystem : MonoBehaviour
 {
+    [Header("Result Charater List")]
     [SerializeField] private GameObject[] characterList;
+    [SerializeField] private GameObject resultObject;
+
+
+    [Header("Result Charater List_10")]
+    [SerializeField] private GameObject resultObject_10;
+    [SerializeField] private Dictionary<int, GameObject[]> characterDictionary = new Dictionary<int, GameObject[]>();
+
+    [SerializeField] private GameObject[] characterList_0;
+    [SerializeField] private GameObject[] characterList_1;
+    [SerializeField] private GameObject[] characterList_2;
+    [SerializeField] private GameObject[] characterList_3;
+    [SerializeField] private GameObject[] characterList_4;
+    [SerializeField] private GameObject[] characterList_5;
+    [SerializeField] private GameObject[] characterList_6;
+    [SerializeField] private GameObject[] characterList_7;
+    [SerializeField] private GameObject[] characterList_8;
+    [SerializeField] private GameObject[] characterList_9;
+
+    [Header("Draw Gold")]
     [SerializeField] private int drawGold;
 
+    [Header("UI")]
     [SerializeField] private GameObject drawPopUP;
     [SerializeField] private GameObject errorPopUp;
+    [SerializeField] private GameObject updatePopUp;
+    [SerializeField] private GameObject updatePopUp_10;
 
+    [Header("Paticle System")]
+    [SerializeField] private ParticleSystem chargeParticle_0;
+    [SerializeField] private ParticleSystem chargeParticle_1;
+    [SerializeField] private ParticleSystem bangParticle_0;
+    [SerializeField] private ParticleSystem bangParticle_1;
+    [SerializeField] private GameObject finalPaticle;
+
+    private Coroutine drawAnimation;
+
+
+    private void Start()
+    {
+        SetCharacterDictionary();
+    }
+
+    private void SetCharacterDictionary()
+    {
+        Debug.Log("리셋");
+        characterDictionary.Add(0, characterList_0);
+        characterDictionary.Add(1, characterList_1);
+        characterDictionary.Add(2, characterList_2);
+        characterDictionary.Add(3, characterList_3);
+        characterDictionary.Add(4, characterList_4);
+        characterDictionary.Add(5, characterList_5);
+        characterDictionary.Add(6, characterList_6);
+        characterDictionary.Add(7, characterList_7);
+        characterDictionary.Add(8, characterList_8);
+        characterDictionary.Add(9, characterList_9);
+    }
 
     public void ClickDrawBtn()
     {
         ResetArray();
-
-        if (!CanDraw()) return;
+        CloseUpdatePopUp();
+        CloseUpdatePopUp_10();
+        //if (!CanDraw()) return;
 
         int a = Random.Range(0, characterList.Length);
-
         characterList[a].SetActive(true);
+        UpdateUserCharaterData(a);
 
-        switch(a)
+
+
+        if (drawAnimation != null)
+        {
+            StopCoroutine(drawAnimation);
+        }
+        drawAnimation = StartCoroutine(DrawAnimation(1));
+    }
+
+    private void UpdateUserCharaterData(int index)
+    {
+        switch (index)
         {
             case 0:
                 //중복된 캐릭터 보유 메서드
+                AddDrawCharater(101);
                 break;
             case 1:
                 AddDrawCharater(102);
@@ -89,6 +155,51 @@ public class DrawUISystem : MonoBehaviour
         }
     }
 
+    public void ClickDrawBtn_10()
+    {
+        
+        //Task.Run(() => ResetDictionary());
+        
+        CloseUpdatePopUp();
+        CloseUpdatePopUp_10();
+
+        //if (!CanDraw()) return;
+
+        ResetDictionary();
+        SetUserData_10();
+
+
+        if (drawAnimation != null)
+        {
+            StopCoroutine(drawAnimation);
+        }
+        drawAnimation = StartCoroutine(DrawAnimation(10));
+    }
+
+
+    private IEnumerator DrawAnimation(int value)
+    {
+        chargeParticle_0.Play();
+        chargeParticle_1.Play();
+        yield return new WaitForSeconds(3f);
+        chargeParticle_0.Stop();
+        chargeParticle_1.Stop();
+        bangParticle_0.Play();
+        bangParticle_1.Play();
+        finalPaticle.SetActive(true);
+
+        if(value == 1)
+        {
+            resultObject.SetActive(true);
+        }
+        else
+        {
+            resultObject_10.SetActive(true);
+        }
+
+    }
+
+
     private bool CanDraw()
     {
         if (DBManager.instance.user.goods["gold"] > drawGold) return true;
@@ -102,19 +213,65 @@ public class DrawUISystem : MonoBehaviour
 
     private void AddDrawCharater(int index)
     {
-        if (HaveUserIndexCharater(index))
+/*        if (HaveUserIndexCharater(index))
         {
             //중복된 캐릭터 보유 메서드
         }
         else
         {
             DBManager.instance.AddCharacter(index);
+        }*/
+    }
+
+    private void SetUserData_10()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            //랜덤으로 인덱스 뽑기
+            int a = Random.Range(0, characterList.Length);
+
+            //0~9 리스트 SetActiveTrue;
+            characterDictionary[i][a].SetActive(true);
+
+
+            //0~9 리스트 AddDrawCharater;
+            UpdateUserCharaterData(a);
         }
+    }
+
+    private void ResetDictionary()
+    {
+        chargeParticle_0.Stop();
+        chargeParticle_1.Stop();
+        finalPaticle.SetActive(false);
+        bangParticle_0.Stop();
+        bangParticle_1.Stop();
+        resultObject.SetActive(false);
+        resultObject_10.SetActive(false);
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                characterDictionary[i][j].SetActive(false);
+            }
+        }
+
+        //if (!CanDraw()) return;
+
     }
 
     private void ResetArray()
     {
-        for(int i = 0; i < characterList.Length; i++)
+        chargeParticle_0.Stop();
+        chargeParticle_1.Stop();
+        finalPaticle.SetActive(false);
+        bangParticle_0.Stop();
+        bangParticle_1.Stop();
+        resultObject.SetActive(false);
+        resultObject_10.SetActive(false);
+
+        for (int i = 0; i < characterList.Length; i++)
         {
             characterList[i].SetActive(false);
         }
@@ -127,11 +284,32 @@ public class DrawUISystem : MonoBehaviour
         {
             if(character.index == index)
             {
+                RaiseCharaterCount(character);
                 return true;
             }
         }
 
         return false;
+    }
+
+    public void OpenUpdatePopUp()
+    {
+        updatePopUp.SetActive(true);
+    }
+
+    public void CloseUpdatePopUp()
+    {
+        updatePopUp.SetActive(false);
+    }
+
+    public void OpenUpdatePopUp_10()
+    {
+        updatePopUp_10.SetActive(true);
+    }
+
+    public void CloseUpdatePopUp_10()
+    {
+        updatePopUp_10.SetActive(false);
     }
 
     public void CloseErrorPopUp()
@@ -141,7 +319,13 @@ public class DrawUISystem : MonoBehaviour
 
     public void CloseDrawPopUp()
     {
+        Debug.Log("drawPopup Close");
         ResetArray();
-        gameObject.SetActive(false);
+        drawPopUP.SetActive(false);
+    }
+
+    private void RaiseCharaterCount(Character character)
+    {
+        //character.count + 1;
     }
 }
