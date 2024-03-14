b799b115f0e746e24dd1e0145a715947f4049988
Sujiii1 <147810556+Sujiii1@users.noreply.github.com>
2024-03-14 17:49:33 +0900

    [Feat] Boss State

--- Assets/3.Script/SuJin/Boss/BossControll.cs
+++ Assets/3.Script/SuJin/Boss/BossControll.cs
@@ -1,20 +1,38 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossControll : MonoBehaviour
 {
+    public EventHandler onBossHPSlide;
+    public EventHandler onBossDamage;
+
     [SerializeField] HorizontalPlayer horizontalPlayer;
     [SerializeField] Rigidbody2D rb;
-
     [SerializeField] Vector2 screenBound;
 
+    [SerializeField] GameObject bossRed;
+
+    //HP
+    public float maxHealth;
+    public float currentHealth;
+    public float damage;
+
+
+    [SerializeField] Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Start()
     {
         horizontalPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<HorizontalPlayer>();
         rb = GetComponent<Rigidbody2D>();
 
+        currentHealth = maxHealth;
     }
 
     private void FixedUpdate()
@@ -27,6 +45,7 @@ public class BossControll : MonoBehaviour
         ClampScreen();
     }
 
+    #region [Player Follow]
     public void ClampScreen()
     {
         screenBound = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
@@ -39,6 +58,42 @@ public class BossControll : MonoBehaviour
     {
         rb.velocity = horizontalPlayer.GetRigidbody2D().velocity;
         transform.position = new Vector2(horizontalPlayer.gameObject.transform.position.x, transform.position.y);
+    }
+    #endregion
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Star"))
+        {
+            animator.SetTrigger("BossHurt");
+
+            BossDamage();
+            onBossHPSlide?.Invoke(this, EventArgs.Empty);   //Damage
+        }
+
+    }
+    public void BossDamage()
+    {
+        currentHealth -= damage;
+        onBossDamage?.Invoke(this, EventArgs.Empty);
+
+         if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            //BossDie(); // 만들어 주기
+            return;
+        }
+        else if(currentHealth <= 30)
+        {
+            bossRed.gameObject.SetActive(true);
+            //Die 메서드
+        }
+        else if(currentHealth <= 50)
+        {
+            animator.SetTrigger("BossRage");
+        }
+      
     }
 }
 
