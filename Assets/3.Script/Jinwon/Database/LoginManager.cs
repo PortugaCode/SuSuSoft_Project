81593d99bfa140e3231e74f7f14dd1376f1012d0
Jinwon Kim <jinwon0707@gmail.com>
2024-03-15 10:28:03 +0900

    [Feat] Add Google Login Method

--- Assets/3.Script/Jinwon/Database/LoginManager.cs
+++ Assets/3.Script/Jinwon/Database/LoginManager.cs
@@ -49,6 +49,24 @@ public class LoginManager : MonoBehaviour
         }
     }
 
+    public void StartGoogleLogin()
+    {
+        TheBackend.ToolKit.GoogleLogin.Android.GoogleLogin(GoogleLoginCallback);
+    }
+
+    private void GoogleLoginCallback(bool isSuccess, string errorMessage, string token)
+    {
+        if (isSuccess == false)
+        {
+            Debug.LogError(errorMessage);
+            return;
+        }
+
+        Debug.Log("구글 토큰 : " + token);
+        var bro = Backend.BMember.AuthorizeFederation(token, FederationType.Google);
+        Debug.Log("페데레이션 로그인 결과 : " + bro);
+    }
+
     public void Login()
     {
         // 캐싱
