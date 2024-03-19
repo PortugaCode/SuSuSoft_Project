174f752c8d46fffb1a9cc4a72ae491e429f2ff3b
Jinwon Kim <jinwon0707@gmail.com>
2024-03-19 10:11:33 +0900

    [Feat] Stage Total Reward
    총합 보상 구현, 스테이지 클리어 UI 수정

--- Assets/3.Script/Jinwon/UI/StageSelectControl.cs
+++ Assets/3.Script/Jinwon/UI/StageSelectControl.cs
@@ -11,19 +11,54 @@ public class StageSelectControl : MonoBehaviour
     [SerializeField] private Sprite starIcon_Off;
     [SerializeField] private GameObject[] starIcons;
 
+    [Header("CheckPoint")]
+    [SerializeField] private Image progressBar;
+    [SerializeField] private TMP_Text[] countTexts;
+    private int[] checkCounts = { 10, 20, 30 };
+
     private void OnEnable()
     {
+        int count = 0;
+
         for (int i = 0; i < 5; i++) // i < 스테이지 개수 로 변경 필요
         {
             for (int j = 0; j < 3; j++)
             {
-                if (DBManager.instance.user.clearInfo[i,j] == 1)
+                if (DBManager.instance.user.clearInfo[i, j] == 1)
                 {
                     starIcons[i].transform.GetChild(j).GetComponent<Image>().sprite = starIcon_On;
+                    count += 1;
                 }
                 else
                 {
                     starIcons[i].transform.GetChild(j).GetComponent<Image>().sprite = starIcon_Off;
+                }
+            }
+        }
+
+        progressBar.fillAmount = (float)(count / 5);
+
+        for (int i = 0; i < 3; i++)
+        {
+            countTexts[i].text = checkCounts[i].ToString();
+
+            if (count >= checkCounts[i] && DBManager.instance.user.stageTotalInfo[i] == 0)
+            {
+                DBManager.instance.user.stageTotalInfo[i] = 1;
+
+                switch (i)
+                {
+                    case 0:
+                        DBManager.instance.user.goods["gold"] += 1000;
+                        break;
+                    case 1:
+                        DBManager.instance.user.goods["ruby"] += 1000;
+                        break;
+                    case 2:
+                        DBManager.instance.user.tokens[Random.Range(0, 10)] += 1;
+                        break;
+                    default:
+                        break;
                 }
             }
         }
